6bee692611c0414a94c1db628eaf3c3782ff2a4f
andreagioelepiazza12 <andreagioelepiazza@gmail.com>
2024-10-24 17:34:13 +0200

    commit
    variabili dinamiche e css

--- resultpage/result.js
+++ resultpage/result.js
@@ -1,10 +1,22 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 
+const numDomande = 10;
+const corrette = 6;
+const sbagliate = 4;
+
+function calcolaPercentuale (num1, num2){
+let result = num1 * num2;
+result = result*100;
+result = result/100;
+return result;
+} 
+
+
         // Dati per il grafico
         const data = {
             labels: ['Corrette', 'Sbagliate'],
             datasets: [{
-                data: [75, 25], // Percentuali
+                data: [calcolaPercentuale(corrette,numDomande), calcolaPercentuale(sbagliate,numDomande)], // Percentuali
                 backgroundColor: ['#00e0f0', '#a64d79'], // Colori per corrette e sbagliate
                 hoverOffset: 4,
                 borderWidth: 0
@@ -28,3 +40,9 @@ const ctx = document.getElementById('myChart').getContext('2d');
         button.addEventListener("click", function (){
             window.location.href ="../feedbackpage/index (2)(1).html"
         })
+
+        const correct = document.getElementById("correctPerc")
+        const wrong = document.getElementById("wrongPerc")
+
+        correct.textContent = "Correct " + calcolaPercentuale(corrette,numDomande) + "%";
+        wrong.textContent = "Wrong " + calcolaPercentuale(sbagliate, numDomande) + "%";
